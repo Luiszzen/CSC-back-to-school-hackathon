befa31599e6c59d260e198195faa4201fcc949ec
dokxt <dodisiok248@gmail.com>
2026-09-20 18:51:34 -0500

    Add sayHello function and button click handler

---
+++ c.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+  return "Hello, " + name + "!";
+}
+
+// Find the elements from the HTML (using their id)
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
+
+// When the button is clicked, do something:
+button.addEventListener("click", () => {
+  message.textContent = sayHello("world") + " Time: " + new Date().toLocaleTimeString();
+});
